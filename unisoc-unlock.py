4465ad3f81fc92a57ff47c38d8a129a20f44d6a8
Patrick Huesmann <info@patrick-huesmann.de>
2023-10-15 10:14:17 +0200

    Add signature calculation

--- unisoc-unlock.py
+++ unisoc-unlock.py
@@ -2,6 +2,10 @@
 
 import sys
 from adb import fastboot, usb_exceptions
+from Cryptodome.Signature import PKCS1_v1_5
+from Cryptodome.Hash import SHA256
+from Cryptodome.PublicKey import RSA
+import base64
 
 def info_cb(s):
     print(f'info_cb: {s}')
@@ -17,6 +21,13 @@ class OemIdToken:
             self.id = fb_msg.message.decode('utf-8').strip()
 
         self.n += 1
+
+
+def sign_token(tok, key_file):
+    priv_key = RSA.importKey(open(key_file).read())
+    h = SHA256.new(tok)
+    signature = PKCS1_v1_5.new(priv_key).sign(h)
+    return signature
 
 def main():
     try:
@@ -36,10 +47,16 @@ def main():
         print(f'Fastboot error {str(e)}')
         sys.exit(1)
 
-    print(oem_id.id)
+    id = oem_id.id.ljust(2*64, '0')
+    print(id)
     dev.Close()
 
 
 if __name__ == '__main__':
-    main()
+    oem_id = '52473430354d3032333736373036' 
+    id = oem_id.ljust(2*64, '0')
+    id_raw = base64.b16decode(id, casefold=True)
+    # print(id_raw)
+    sgn = sign_token(id_raw, 'rsa4096_vbmeta.pem')
+    open('sig.bin', 'wb').write(sgn)
 
